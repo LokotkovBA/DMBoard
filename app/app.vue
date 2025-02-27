ba489ba86e279027bb6c79a2d3dc6435df8ab3e0
LokotkovBA <woweuro2010@gmail.com>
2025-02-28 02:35:05 +0300

    initial commit

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+useHead({
+    bodyAttrs: {
+        class: 'bg-slate-900',
+    },
+});
+</script>
+
+<template>
+    <div class="mx-2 flex flex-wrap">123</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,27 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    future: {
+        compatibilityVersion: 4,
+    },
+    compatibilityDate: '2024-11-01',
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxt/eslint',
+        '@pinia/nuxt',
+        '@uploadthing/nuxt',
+        'nuxt-security',
+    ],
+    runtimeConfig: {
+        uploadthingToken: process.env.UPLOADTHING_TOKEN,
+    },
+
+    security: {
+        corsHandler: {
+            origin: 'https://www.owlbear.rodeo',
+        },
+    },
+    devServer: {
+        cors: { origin: ['https://www.owlbear.rodeo'] },
+    },
+});
